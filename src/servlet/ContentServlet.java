d48311d418baf099e22a63f9f6b32ed287fa19a7
iamusayev <iamusayev1337@gmail.com>
2022-06-23 20:29:39 +0400

    added ContentServlet

---
+++ src/servlet/ContentServlet.java
@@ -0,0 +1,19 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import util.JspHelper;
+
+import java.io.IOException;
+
+@WebServlet("/content")
+public class ContentServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(JspHelper.get("content")).forward(req,resp);
+    }
+}
