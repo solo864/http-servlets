9927bd1bc667adee19e95f3bb6665b66a9a9213a
iamusayev <iamusayev1337@gmail.com>
2022-06-23 19:01:04 +0400

    added FlightEntity

---
+++ src/entity/FlightEntity.java
@@ -0,0 +1,25 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import model.enums.FlightStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class FlightEntity {
+
+    private Long id;
+    private String flightNo;
+    private LocalDateTime departureDate;
+    private String departureAirportCode;
+    private LocalDateTime arrivalDate;
+    private String arrivalAirportCode;
+    private FlightStatus status;
+
+}
