6604ee22e321cf4f8fc9212ca28915beb7d6738b
iamusayev <iamusayev1337@gmail.com>
2022-06-23 10:11:54 +0400

    added DownloadServlet

---
+++ src/servlet/DownloadServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+
+@WebServlet("/check-downloads")
+public class DownloadServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setHeader("Content-Disposition", "attachment; \"filename.txt\"");
+
+        try (var writer = resp.getOutputStream()) {
+            InputStream resourceAsStream = DownloadServlet.class.getClassLoader().getResourceAsStream("application.properties");
+            writer.write(resourceAsStream.readAllBytes());
+        }
+    }
+}
