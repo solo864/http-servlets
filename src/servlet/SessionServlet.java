3f032c96d5d9fd4ce5026cc7a869eb94c0e8276e
iamusayev <iamusayev1337@gmail.com>
2022-06-24 11:59:02 +0400

    added flights

---
+++ src/servlet/SessionServlet.java
@@ -0,0 +1,31 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet("/sessions")
+public class SessionServlet extends HttpServlet {
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        resp.setContentType("text/html");
+
+        HttpSession session = req.getSession();
+
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write("<h1>I CREATED SESSION IN YOUR BROWSER CHECK IT</h1    >");
+        }
+
+    }
+}
