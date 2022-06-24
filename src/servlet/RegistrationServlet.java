55fe13af12e9b064f93a20b8d0cccf196db10848
iamusayev <iamusayev1337@gmail.com>
2022-06-24 17:40:35 +0400

    added RegistrationServlet

---
+++ src/servlet/RegistrationServlet.java
@@ -0,0 +1,25 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import util.JspHelper;
+
+import java.io.IOException;
+
+@WebServlet("/reg")
+public class RegistrationServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(JspHelper.get("reg"))
+                .forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+    }
+}
