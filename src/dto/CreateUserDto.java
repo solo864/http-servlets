565168ef48804bf01947165cdf83115b5c3e346a
iamusayev <iamusayev1337@gmail.com>
2022-06-26 20:19:38 +0400

    added Create User Dto

---
+++ src/dto/CreateUserDto.java
@@ -0,0 +1,17 @@
+package dto;
+
+import lombok.Builder;
+import lombok.Value;
+
+@Value
+@Builder
+public class CreateUserDto {
+
+    private String name;
+    private String password;
+    private String email;
+    private String birthday;
+    private String role;
+    private String gender;
+
+}
