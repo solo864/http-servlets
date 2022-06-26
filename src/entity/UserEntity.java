5a1872a2a30dbf158a1f0082e9bbc0f7c26974e3
iamusayev <iamusayev1337@gmail.com>
2022-06-26 19:54:48 +0400

    added user entity

---
+++ src/entity/UserEntity.java
@@ -0,0 +1,27 @@
+package entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import model.Gender;
+import model.Role;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserEntity {
+
+    private Integer id;
+    private String name;
+    private String email;
+    private String password;
+    private LocalDate birthday;
+    private Gender gender;
+    private Role role;
+
+}
