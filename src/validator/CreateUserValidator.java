2b8efa0c3f7fbcb63bd713fc880d10739c376cc6
iamusayev <iamusayev1337@gmail.com>
2022-06-26 20:20:00 +0400

    added Create User Validator

---
+++ src/validator/CreateUserValidator.java
@@ -0,0 +1,30 @@
+package validator;
+
+import dto.CreateUserDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import model.Gender;
+import model.Role;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CreateUserValidator implements Validator<CreateUserDto> {
+
+    private static final CreateUserValidator INSTANCE = new CreateUserValidator();
+
+
+    @Override
+    public ValidationResult isValid(CreateUserDto object) {
+        ValidationResult validationResult = new ValidationResult();
+        if (object.getRole() == null || Role.valueOf(object.getRole()) == null) {
+            validationResult.add(Error.of("invalid role", "role is invalid"));
+        }
+        if (object.getGender() == null || Gender.valueOf(object.getGender()) == null) {
+            validationResult.add(Error.of("invalid gender", "gender is invalid"));
+        }
+        return validationResult;
+    }
+
+    public static CreateUserValidator getInstance() {
+        return INSTANCE;
+    }
+}
