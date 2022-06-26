f9dc1cb79414a00c0376ee6ea90564e718384499
iamusayev <iamusayev1337@gmail.com>
2022-06-26 20:20:21 +0400

    added LocalDateFormatter

---
+++ src/util/LocalDateFormatter.java
@@ -0,0 +1,32 @@
+package util;
+
+import lombok.experimental.UtilityClass;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Optional;
+
+@UtilityClass
+public class LocalDateFormatter {
+
+    private static final String PATTERN = "yyyy/MM/dd";
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(PATTERN);
+
+
+    public static LocalDate format(String date) {
+        return LocalDate.parse(date, FORMATTER);
+    }
+
+
+    public boolean isValid(String date) {
+        try {
+            return Optional.ofNullable(date)
+                    .map(LocalDateFormatter::isValid)
+                    .isPresent();
+        } catch (DateTimeParseException ex) {
+            return false;
+        }
+    }
+
+}
