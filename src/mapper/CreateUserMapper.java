48cf17d57a93cf8c6724547cfafb251e4a1f2ddc
iamusayev <iamusayev1337@gmail.com>
2022-06-26 20:19:48 +0400

    added Create User Mapper

---
+++ src/mapper/CreateUserMapper.java
@@ -0,0 +1,32 @@
+package mapper;
+
+import dto.CreateUserDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import model.Gender;
+import model.Role;
+import util.LocalDateFormatter;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CreateUserMapper implements Mapper<UserEntity, CreateUserDto> {
+
+    private static final CreateUserMapper INSTANCE = new CreateUserMapper();
+
+
+    public static CreateUserMapper getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public UserEntity mapTo(CreateUserDto object) {
+        return UserEntity.builder()
+                .name(object.getName())
+                .email(object.getEmail())
+                .password(object.getPassword())
+                .gender(Gender.valueOf(object.getGender()))
+                .role(Role.valueOf(object.getRole()))
+                .birthday(LocalDateFormatter.format(object.getBirthday()))
+
+                .build();
+    }
+}
