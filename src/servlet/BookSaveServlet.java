2211c986a01ab0044e4c072eb5984e6292a7b846
iamusayev <iamusayev1337@gmail.com>
2022-06-27 12:24:19 +0400

    added Book save servlet

--- src/servlet/BookSaveServlet.java
+++ src/servlet/BookSaveServlet.java
@@ -1,4 +1,4 @@
-package http;
+package servlet;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -6,23 +6,18 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import service.BookService;
+import util.JspHelper;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-@WebServlet("/first")
-public class FirstServlet extends HttpServlet {
+@WebServlet("/books-save")
+public class BookSaveServlet extends HttpServlet {
 
-    private BookService bookService = BookService.getInstance();
+    private final BookService bookService = BookService.getInstance();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try (var writer = resp.getWriter()) {
-            writer.write("<h1> HEY EVERYONE </h1>");
-        }
+        req.getRequestDispatcher(JspHelper.get("savebook"))
+                .forward(req, resp);
     }
 }
