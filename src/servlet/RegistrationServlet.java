cc3e212d0d794e59a35e72fc5a5e96136e886347
iamusayev <iamusayev1337@gmail.com>
2022-06-27 12:24:41 +0400

    added RegistrationServlet

--- src/servlet/RegistrationServlet.java
+++ src/servlet/RegistrationServlet.java
@@ -1,10 +1,13 @@
 package servlet;
 
+import dto.CreateUserDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import model.Gender;
+import model.Role;
 import util.JspHelper;
 
 import java.io.IOException;
@@ -14,16 +17,22 @@ public class RegistrationServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher(JspHelper.get("reg"))
+        req.setAttribute("roles", Role.values());
+        req.setAttribute("genders", Gender.values());
+        req.getRequestDispatcher(JspHelper.get("registration"))
                 .forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String name = req.getParameter("username");
-        String password = req.getParameter("password");
-        String birthDay = req.getParameter("birthday");
-        String role = req.getParameter("role");
-        String gender = req.getParameter("gender");
+        CreateUserDto userDto = CreateUserDto.builder()
+                .name(req.getParameter("name"))
+                .password(req.getParameter("password"))
+                .birthday(req.getParameter("birthday"))
+                .role(req.getParameter("role"))
+                .email(req.getParameter("email"))
+                .gender(req.getParameter("gender"))
+                .build();
+
     }
 }
