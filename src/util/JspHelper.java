ca99e646be0fd59f3523c494608f88222ffe3722
iamusayev <iamusayev1337@gmail.com>
2022-07-02 18:29:36 +0400

    added JspHelper

--- src/util/JspHelper.java
+++ src/util/JspHelper.java
@@ -5,6 +5,7 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class JspHelper {
 
+
     public static final String JSP_FORMAT = "/WEB-INF/jsp/%s.jsp";
 
     public static String get(String jspName){
