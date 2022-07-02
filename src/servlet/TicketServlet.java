91ab40edc1ce93428834a20c5ea9f03eab4a49d5
iamusayev <iamusayev1337@gmail.com>
2022-07-02 18:33:06 +0400

    added TicketServlet

--- src/servlet/TicketServlet.java
+++ src/servlet/TicketServlet.java
@@ -14,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 @WebServlet("/tickets")
 public class TicketServlet extends HttpServlet {
 
+
     private final TicketService ticketService = TicketService.getInstance();
 
     @Override
