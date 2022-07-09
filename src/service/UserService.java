31f0ef0345931e1c0cd2263d3d9245576f3a9b90
iamusayev <iamusayev1337@gmail.com>
2022-07-09 14:26:13 +0400

    added UserService

--- src/service/UserService.java
+++ src/service/UserService.java
@@ -2,22 +2,36 @@ package service;
 
 import dao.UserDao;
 import dto.CreateUserDto;
+import entity.UserEntity;
+import exception.ValidationException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import mapper.UserMapper;
+import validator.CreateUserValidator;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserService {
 
     private static final UserService INSTANCE = new UserService();
 
+
+    private final CreateUserValidator userValidator = getInstance().userValidator;
+    ;
+    private final UserMapper userMapper = UserMapper.getInstance();
+    private final ImageService imageService = ImageService.getInstance();
     private final UserDao userDao = UserDao.getInstance();
 
 
-    public Integer saveUser(CreateUserDto userDto) {
-        //validation
-        //mapper
-        //save
-        return null;
+    @SneakyThrows
+    public void saveUser(CreateUserDto userDto) {
+        var validationResult = userValidator.isValid(userDto);
+        if (!validationResult.isValid()) {
+            throw new ValidationException(validationResult.getErrors());
+        }
+        final UserEntity userEntity = userMapper.MapCreateDtoToEntity(userDto);
+        imageService.upload(userEntity.getImage(), userDto.getImage().getInputStream());
+        userDao.save(userEntity);
     }
 
 
