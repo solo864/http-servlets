0100ec8289fce0c64816873ececfd551a50ff3f5
iamusayev <iamusayev1337@gmail.com>
2022-07-09 11:20:24 +0400

    added CreateUserValidator

--- src/validator/CreateUserValidator.java
+++ src/validator/CreateUserValidator.java
@@ -5,6 +5,7 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import model.enums.Gender;
 import model.enums.Role;
+import util.LocalDateFormatter;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CreateUserValidator implements Validator<CreateUserDto> {
@@ -15,11 +16,14 @@ public class CreateUserValidator implements Validator<CreateUserDto> {
     @Override
     public ValidationResult isValid(CreateUserDto object) {
         ValidationResult validationResult = new ValidationResult();
-        if (object.getRole() == null || Role.valueOf(object.getRole()) == null) {
-            validationResult.add(Error.of("invalid role", "role is invalid"));
-        }
-        if (object.getGender() == null || Gender.valueOf(object.getGender()) == null) {
-            validationResult.add(Error.of("invalid gender", "gender is invalid"));
+//        if (object.getRole() == null || Role.valueOf(object.getRole()) == null) {
+//            validationResult.add(Error.of("invalid role", "role is invalid"));
+//        }
+//        if (object.getGender() == null || Gender.valueOf(object.getGender()) == null) {
+//            validationResult.add(Error.of("invalid gender", "gender is invalid"));
+//        }
+        if (!LocalDateFormatter.isValid(object.getBirthday())){
+            validationResult.add(Error.of("invalid birthday", "birthday is invalid"));
         }
         return validationResult;
     }
