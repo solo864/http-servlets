51314471e24ab01ad02ad55012e478c47c0a4ba5
iamusayev <iamusayev1337@gmail.com>
2022-07-09 14:26:21 +0400

    added ValidationException

---
+++ src/exception/ValidationException.java
@@ -0,0 +1,18 @@
+package exception;
+
+import lombok.Getter;
+import validator.Error;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationException extends RuntimeException{
+
+    @Getter
+    private final List<Error> errors ;
+
+
+    public ValidationException(List<Error> errors) {
+        this.errors = errors;
+    }
+}
