e36d7afc0cf35a69f904e71e1e850dcc476f75c6
iamusayev <iamusayev1337@gmail.com>
2022-07-09 14:26:00 +0400

    added UserMapper

--- src/mapper/UserMapper.java
+++ src/mapper/UserMapper.java
@@ -1,6 +1,7 @@
 package mapper;
 
 import dto.CreateUserDto;
+import entity.UserEntity;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import model.enums.Gender;
@@ -8,27 +9,28 @@ import model.enums.Role;
 import util.LocalDateFormatter;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class CreateUserMapper implements Mapper<UserEntity, CreateUserDto> {
+public class UserMapper implements Mapper<UserEntity, CreateUserDto> {
 
 
 
-    private static final CreateUserMapper INSTANCE = new CreateUserMapper();
+    private static final UserMapper INSTANCE = new UserMapper();
 
+    private static final String IMAGE_FOLDER = "users/";
 
-    public static CreateUserMapper getInstance() {
+    public static UserMapper getInstance() {
         return INSTANCE;
     }
 
     @Override
-    public UserEntity mapTo(CreateUserDto object) {
+    public UserEntity MapCreateDtoToEntity(CreateUserDto object) {
         return UserEntity.builder()
                 .name(object.getName())
                 .email(object.getEmail())
                 .password(object.getPassword())
                 .gender(Gender.valueOf(object.getGender()))
                 .role(Role.valueOf(object.getRole()))
+                .image(IMAGE_FOLDER+object.getImage().getSubmittedFileName())
                 .birthday(LocalDateFormatter.format(object.getBirthday()))
-
                 .build();
     }
 }
