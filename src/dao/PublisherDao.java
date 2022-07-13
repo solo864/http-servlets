1d2eb6dcc0abe823defabb1b923beeecd2e77061
iamusayev <iamusayev1337@gmail.com>
2022-07-13 12:36:33 +0400

    added PublisherDao

--- src/dao/PublisherDao.java
+++ src/dao/PublisherDao.java
@@ -14,6 +14,7 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PublisherDao {
 
+
     private static final PublisherDao INSTANCE = new PublisherDao();
 
     private static final String FIND_ALL = """
